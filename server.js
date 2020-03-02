5919babd901d0203f91c52d1107f527b3471758f
Dido Miyalu <dieudonne.miyalu@academy.kinshasadigital.com>
2020-03-02 20:16:29 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+
+
+
+//middlewares
+app.use(morgan('combined'))
+    .use(express.static(__dirname + '/public'));
+
+
+//moteur de templates
+app.set('view engine', 'ejs');
+
+// gestion des routes
+app.get('/', function(request, response) {
+        response.render('accueil')
+    })
+    .get('/projets', function(request, response) {
+        response.render('projets')
+    })
+    .get('/projets_details', function(request, response) {
+        response.render('projets_details')
+    })
+    .get('/investir', function(request, response) {
+        response.render('commentInvestir')
+    })
+    .get('/leverDesFonds', function(request, response) {
+        response.render('leverDesFonds')
+    })
+    .get('/contact', function(request, response) {
+        response.render('contact')
+    });
+//gestion des erreurs 404
+
+app.use(function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(8080, function() {
+    console.log('Server running at localhost:8080');
+});
